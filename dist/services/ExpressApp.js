c386e924b93ce07a1c6208fa50b119a81eb15a91
Nicolas Marinelli <marinellinicolas@gmail.com>
2024-05-16 09:36:31 -0300

    changes for deplayment

--- dist/services/ExpressApp.js
+++ dist/services/ExpressApp.js
@@ -14,11 +14,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const routes_1 = require("../routes");
-const path_1 = __importDefault(require("path"));
 exports.default = (app) => __awaiter(void 0, void 0, void 0, function* () {
     app.use(express_1.default.json());
     app.use(express_1.default.urlencoded({ extended: true }));
-    app.use("/images", express_1.default.static(path_1.default.join(__dirname, "images")));
+    app.get("./", (req, res) => {
+        res.send("Hola Monudo");
+    });
     app.use('/admin', routes_1.AdminRoute);
     return app;
 });
--- src/services/ExpressApp.ts
+++ src/services/ExpressApp.ts
@@ -9,9 +9,11 @@ export default async (app: Application)=>{
 
     app.use(express.json());
     app.use(express.urlencoded({extended:true}))
-    app.use("/images",express.static(path.join(__dirname,"images")))
+
     
-    
+    app.get("./",(req,res)=>{
+        res.send("Hola Monudo")
+    })
     app.use('/admin', AdminRoute)
       
     return app
